937f8cbb06afef1447cd1677a6e9f434d742b532
Daniel Ilkovich <daniel+github@betaworks.com>
2016-03-08 17:07:25 -0500

    initial

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var   _ = require('lodash')
+  , req = require('superagent')
+  , q   = require('q')
+  , qs  = require('query-string')
+;
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +13,57 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var credentials = dexter.provider('asana').credentials('access_token')
+          , names       = step.input('filter_name').toArray()
+          , id          = step.input('filter_workspace').first()
+          , team        = step.input('filter_team').first()
+          , archived    = step.input('filter_archived').first()
+          , self        = this
+          , query       = {}
+        ;
+
+        setIfExists(query, 'workspace', id);
+        setIfExists(query, 'team', team);
+        setIfExists(query, 'archived', archived);
+
+        //normalize
+        names = _.map(names, function(name) { return name.toLowerCase(); });
+
+        query = '?'+qs.stringify(query);
+
+        console.log('query', query);
+
+        var request = req.get('https://app.asana.com/api/1.0/projects'+query)
+                         .set('Authorization', 'Bearer '+credentials)
+                         .type('json')
+                     ;
+
+        promisify(request, 'end', 'body.data')
+           .then(function(projects) {
+               if(names.length) 
+                   self.complete( _.filter(projects, function(project) { return names.indexOf(project.name.toLowerCase()) !== -1; }) );
+               else 
+                   self.complete(projects);
+           })
+           .catch(this.fail.bind(this));
     }
 };
+
+function promisify(scope, call, path) {
+    var deferred = q.defer(); 
+
+    scope[call](function(err, result) {
+        return err
+          ? deferred.reject(err)
+          : deferred.resolve(_.get(result, path))
+        ;
+    });
+
+    return deferred.promise;
+}
+
+function setIfExists(obj, key, value) {
+    if(value !== null & value !== undefined) {
+        obj[key] = value;
+    }
+}
